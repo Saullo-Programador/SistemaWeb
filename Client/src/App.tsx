8e1a90f416200e9c5671dc7aa5cacc22c488dd6b
Saullo-Programador <saullopaulodantasfelipe8@gmail.com>
2024-02-19 22:26:12 -0300

    autenticação de usuario

---
+++ Client/src/App.tsx
@@ -0,0 +1,13 @@
+import RouterPages from './Routes/RouterPages';
+import GlobalStyle from './Styles/GlobalStyled';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle/>
+      <RouterPages/>
+    </>
+  );
+}
+
+export default App;
